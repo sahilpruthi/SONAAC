2ea591c7fa9a6163337edc0a35b5600c9f7d4d21
sahil <sahilpruthi412@gmail.com>
2017-12-26 00:12:06 -0500

    migration chagnes

--- db/migrate/20171218094417_add_column_for_user.rb
+++ db/migrate/20171218094417_add_column_for_user.rb
@@ -1,7 +1,7 @@
 class AddColumnForUser < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :user_name, :string
-    add_column :users, :phone_number, :integer, limit: 10
+    add_column :users, :phone_number, :integer
     add_column :users, :emergency_number, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171218100924) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "drivers", force: :cascade do |t|
     t.string   "name"
     t.integer  "aadhar_number"
@@ -26,23 +29,23 @@ ActiveRecord::Schema.define(version: 20171218100924) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                             default: "", null: false
-    t.string   "encrypted_password",                default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "user_name"
-    t.integer  "phone_number",           limit: 10
+    t.integer  "phone_number"
     t.integer  "emergency_number"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
