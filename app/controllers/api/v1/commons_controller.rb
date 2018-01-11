103697dc15e22c39b448e5b19292e6a10d0b1e6f
sahil <sahilpruthi412@gmail.com>
2018-01-11 22:11:33 +0530

    status true after sending notification

--- app/controllers/api/v1/commons_controller.rb
+++ app/controllers/api/v1/commons_controller.rb
@@ -6,8 +6,9 @@ class Api::V1::CommonsController < ApiController
 		drivers = Driver.near([params[:latitude], params[:longitude]], 2, :units => :km)
     if drivers.present?
 		  Driver.send_notification(drivers.pluck(:fcm_token).compact)
+      render json: { status: true }
     else
-      render json: { status: :false, available_drivers: 'No Driver Available' }
+      render json: { status: :false, available_drivers: 'No Driver Available in your location' }
     end
   end
 
