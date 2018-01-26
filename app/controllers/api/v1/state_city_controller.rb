22a02b90514762d7fd4103d66c2e703d45d5a1c3
komal.pasrija <komal.pasrija@agamitechnologies.com>
2018-01-26 14:59:19 +0530

    state

--- app/controllers/api/v1/state_city_controller.rb
+++ app/controllers/api/v1/state_city_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::StateCityController < ApplicationController
 
 	def get_state
 		states = CS.states(:in)
-		state_json = {}
 		state_array = []
 		states.each do |k, v|
+			state_json = Hash.new
 			state_json[:state_key] = k
 			state_json[:state_value] = v
 			state_array << state_json
