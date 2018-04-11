6b0b97b2bef95f4e88db9f28e7ba2d233af0c94c
sahil <sahilpruthi412@gmail.com>
2018-04-11 21:48:27 +0530

    check error

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -23,6 +23,7 @@ class Vehicle < ApplicationRecord
     spreadsheet = Roo::Spreadsheet.open(file.path)
     header = spreadsheet.sheet('Sheet1').row(1)
     (2..spreadsheet.last_row).each do |i|
+    # (2..1518).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
       if row.dig('name(station_name)') != nil
         station = Station.find_or_create_by(name: row.dig('name(station_name)'))
@@ -40,15 +41,19 @@ class Vehicle < ApplicationRecord
           registration_no = row.dig('registration_no') == 'Not-available' ? row.dig('registration_no') +i.to_s : row.dig('registration_no')
           vehicle_number = row.dig('vehicle_number') == 'Not-available' ? row.dig('vehicle_number') + i.to_s : row.dig('vehicle_number')
           vehicle_number = Vehicle.find_by(vehicle_number: vehicle_number).present? ? vehicle_number + '@' + i.to_s : vehicle_number
-          vehicle = Vehicle.create(model_no: model_no,
-           registration_no: registration_no, vehicle_type:  row.dig('vehicle_type'),
-           vehicle_number: vehicle_number, name: row.dig('name(vehicle_name)'),
-            bus_type: row.dig('bus_type'), service_no: row.dig('Service No'))
-          bus_station  = vehicle.bus_stations.new(is_source: row.dig('is_source'),
-            is_destination: row.dig('is_destination'), arrival_time: arrival_time,
-             departure_time: departure_time, sequence: row.dig('sequence'),
-              station_id: station.id, price: row.dig('Fare'), duration:row.dig('Duration'))
-          bus_station.save!
+          begin
+            vehicle = Vehicle.create(model_no: model_no,
+             registration_no: registration_no, vehicle_type:  row.dig('vehicle_type'),
+             vehicle_number: vehicle_number, name: row.dig('name(vehicle_name)'),
+              bus_type: row.dig('bus_type'), service_no: row.dig('Service No'))
+            bus_station  = vehicle.bus_stations.new(is_source: row.dig('is_source'),
+              is_destination: row.dig('is_destination'), arrival_time: arrival_time,
+               departure_time: departure_time, sequence: row.dig('sequence'),
+                station_id: station.id, price: row.dig('Fare'), duration:row.dig('Duration'))
+            bus_station.save!
+          rescue =>error
+            puts error
+          end
         else
           bus_station  = Vehicle.last.bus_stations.new(is_source: row.dig('is_source'),
               is_destination: row.dig('is_destination'), arrival_time: arrival_time,
