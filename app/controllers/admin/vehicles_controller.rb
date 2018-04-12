90ec1229b2f064a3858916955f8d304e83483877
sahil <sahilpruthi412@gmail.com>
2018-04-12 23:14:35 +0530

    two files upload

--- app/controllers/admin/vehicles_controller.rb
+++ app/controllers/admin/vehicles_controller.rb
@@ -20,8 +20,6 @@ class Admin::VehiclesController < ApplicationController
 	end
 
 	def vehicle_sheet
-	  Vehicle.destroy_all
-	  Station.destroy_all
 	  Vehicle.import(params[:file])
   	redirect_to admin_home_path, notice: "Products imported."
 
